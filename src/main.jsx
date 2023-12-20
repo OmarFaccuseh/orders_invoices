df13158362f6dd2898169e867964309ef02d347f
OmarFaccuseh <omar_fs.07@hotmail.com>
2023-12-19 23:46:09 -0600

    Agregado basename a BrowserRoute

--- src/main.jsx
+++ src/main.jsx
@@ -12,12 +12,10 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename={"orders_invoices"}>
         <CarritoProvider style={{ backgroundColor: 'white' }}>
-          <Routes>
+          <Routes >
             <Route index element={<ListOrders />}  />
-            <Route path='orders_invoices' element={<ListOrders/>} />
- 
             <Route path='orders/:order_id' element={<OrderDetail />} />
             <Route path='facts' element={<Facts />} />
           </Routes>
